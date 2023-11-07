7a095c72235613933276a6c3390c73a34a0d34a5
jai kishan <kumar.jaikishan0@gmail.com>
2023-11-07 13:25:18 +0530

    vbnghn

--- index.js
+++ index.js
@@ -88,8 +88,8 @@ app.post('/photo', async (req, res) => {
 
 
 app.get('/', (req, res) => {
-    app.use(express.static(path.resolve(__dirname, 'client', 'dist')))
-    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+    app.use(express.static(path.resolve(__dirname, 'client', 'build')))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 })
 // }
 
