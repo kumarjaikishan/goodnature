dedf480238023d498e83ddfcc5be4bc3e305fa57
jai kishan <kumar.jaikishan0@gmail.com>
2023-11-07 12:25:40 +0530

    bgfb

--- client/src/pages/photo.jsx
+++ client/src/pages/photo.jsx
@@ -73,8 +73,8 @@ const Photo = ({ notification, setimgine, Api }) => {
                 method: "POST",
                 body: data
             })
-            const resut = await res.json();
-            console.log(resut);
+            const result = await res.json();
+            console.log(result);
             // notification.success("Photo Updated Successfully",1500);
             // setimgine(resuk.imge);
             // navigate('/');
